b98f2b598a417bbb9cc2271d24ad461551b4d63e
YonatanEr <76260092+YonatanEr@users.noreply.github.com>
2023-01-24 17:43:01 +0200

    Fixed Python issues

--- Robot_Kinematics.py
+++ Robot_Kinematics.py
@@ -1,45 +1,47 @@
 import numpy as np
-import matplotlib.pyplot as plt
-class Robot():
-    
+
+class Robot:
+
     def __init__(self):
-        pass
-    ##Robot geometry##
-    theta_min=-np.pi/2
-    theta_max=np.pi/2
-    h=0.3 #m
-    l_1= 1 #m
-    l_2_min=0 #m
-    l_2_max=1 #m
-    l_3_min=0.05 #m
-    l_3_max=0.25 #m
-    ##Robot kinamatics##
-    def Direct_Kinematics(theta,h,l_1,l_2,l_3):
-        c=np.cos(theta)
-        s=np.sin(theta)
-        l=l_1+l_2
-        T=np.array([[-s,c,0,l*c],
-                    [c,s,0,l+s],
-                    [0,0,-1,h-l_3],
-                    [0,0,0,1]])
-        R=T[0:3,0:3]
-        P=T[0:3,3]
-        return T,R,P
-    def Inverse_Kinematics(x,y,z,h,l_1):
-        l_3=h-z
-        theta=np.arctan2(y,x)
-        l_2=(x/(np.cos(theta)))-l_1
+        self.theta_min = -np.pi / 2
+        self.theta_max = np.pi / 2
+        self.h = 0.3  # m
+        self.l_1 = 1  # m
+        self.l_2_min = 0  # m
+        self.l_2_max = 1  # m
+        self.l_3_min = 0.05  # m
+        self.l_3_max = 0.25  # m
+
+    @staticmethod
+    def Direct_Kinematics(theta, h, l_1, l_2, l_3):
+        c = np.cos(theta)
+        s = np.sin(theta)
+        l = l_1 + l_2
+        T = np.array([[-s, c, 0, l * c],
+                      [c, s, 0, l + s],
+                      [0, 0, -1, h - l_3],
+                      [0, 0, 0, 1]])
+        R = T[0:3, 0:3]
+        P = T[0:3, 3]
+        return T, R, P
+
+    @staticmethod
+    def Inverse_Kinematics(x, y, z, h, l_1):
+        l_3 = h - z
+        theta = np.arctan2(y, x)
+        l_2 = (x / (np.cos(theta))) - l_1
         return theta, l_2, l_3
-    def Jacobian(l_1,l_2,theta):
-        c=np.cos(theta)
-        s=np.sin(theta)
-        l=l_1+l_2
-        J_L=np.array([-l*s,c,0],
-                    [l*c,s,0],
-                     [0,0,-1])
-        
-        J_A=np.array([0,0,0],
-                    [0,0,0],
-                     [1,0,0])
+
+    @staticmethod
+    def Jacobian(l_1, l_2, theta):
+        c = np.cos(theta)
+        s = np.sin(theta)
+        l = l_1 + l_2
+        J_L = np.array([-l * s, c, 0],
+                       [l * c, s, 0],
+                       [0, 0, -1])
+
+        J_A = np.array([0, 0, 0],
+                       [0, 0, 0],
+                       [1, 0, 0])
         return J_L, J_A
-    
--- test.py
+++ test.py
@@ -1,15 +1,11 @@
-import math
 import numpy as np
-
-x = np.random.rand(3)
-y = np.random.rand(7)
-print(x)
-print(y)
-
-print()
-print()
-y+=1
-print(x)
-print(y)
-
-
+import matplotlib.pyplot as plt
+def f(x):
+    return x**2*np.exp(-x**2)
+x = np.linspace ( start = 0.    # lower limit
+                , stop = 3      # upper limit
+                , num = 51      # generate 51 points between 0 and 3
+                )
+y = f(x)    # This is already vectorized, that is, y will be a vector!
+plt.plot(x, y)
+plt.show()
